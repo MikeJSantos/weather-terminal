afb76b5aaffd276bcaba9d40fc82ed8180cfe721
Michael Santos <michael.joseph.santos@gmail.com>
2020-03-30 21:28:45 -0400

    finished implementing main

--- Python/Python_Programming_Blueprints/weatherterm/weatherterm/__main__.py
+++ Python/Python_Programming_Blueprints/weatherterm/weatherterm/__main__.py
@@ -11,3 +11,64 @@ def _validate_forecast_args(args):
         print(f'{argparser.prog}: error: {err_msg}', file = sys.stderr)
         sys.exit()
         
+parsers = parser_loader.load('./weatherterm/parsers')
+
+argparser = ArgumentParser(
+    prog = 'weatherterm',
+    description = 'Weather info from weather.com on your terminal'
+)
+
+required = argparser.add_argument_group('required arguments')
+required.add_argument(
+    '-p',
+    '--parser',
+    choices  = parsers.keys(),
+    required = True,
+    dest     = 'parser',
+    help     = ('Specify which parser is going to be used to scrape weather information')
+)
+
+unit_values = [name.title() for name, value in Unit.__members__.items()]
+argparser.add_argument(
+    '-u',
+    '--unit',
+    choices  = unit_values,
+    required = False,
+    dest     = 'unit',
+    help     = ('Specify the unit that will be used to display the temperatures.')
+)
+
+# TODO: group required args together?
+required.add_argument(
+    '-a',
+    '--areacode',
+    dest = 'area_code',
+    help = ('The code area to get the weather broadcast from. It can be obtained at https://weather.com')
+)
+
+argparser.add_argument(
+    '-v',
+    '--version',
+    aciton  = 'version',
+    version = '%(prog)s 1.0'
+)
+
+argparser.add_argument(
+    '-td',
+    '--today',
+    dest   = 'forecast_option',
+    action = 'store_const',
+    const  = ForecastType.TODAY,
+    help   = 'Show the weater forecast for the current day'
+)
+
+args = argparser.parse_args()
+_validate_forecast_args(args)
+
+cls = parsers[args.parser]
+parser = cls()
+
+results = parser.run(args)
+
+for result in results:
+    print(results)

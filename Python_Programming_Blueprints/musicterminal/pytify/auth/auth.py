1f5f37f8a1803a1802fd4f8b06c69de236f00dd1
Michael Santos <michael.joseph.santos@gmail.com>
2020-04-03 13:46:57 -0400

    pytify: implemented Spotify authorization

--- Python_Programming_Blueprints/musicterminal/pytify/auth/auth.py
+++ Python_Programming_Blueprints/musicterminal/pytify/auth/auth.py
@@ -1,5 +1,6 @@
-import requests, base64, json
+import requests, base64, json, os
 from .authorization import Authorization
+from .auth_method import AuthMethod
 from pytify.core import BadRequestError
 
 def get_auth_key(client_id, client_secret):
@@ -36,4 +37,48 @@ def _client_credentials(config):
     return Authorization(access_token, token_type, expires_in, scope, None)
 
 def authenticate(config):
-    return _client_credentials(config)
+    if config.auth_method == AuthMethod.CLIENT_CREDENTIALS:
+        return _client_credentials(config)
+    
+    return _authorization_code(config)
+
+def _refresh_access_token(auth_key, refresh_token):
+    headers = {'Authorization': f'Basic {auth_key}'}
+
+    options = {
+        'refresh_token': refresh_token,
+        'grant_type': 'refresh_token'
+    }
+
+    response = requests.post(
+        'https://accounts.spotify.com/api/token',
+        headers = headers,
+        options = options
+    )
+
+    content = json.loads(response.content.decode('utf-8'))
+
+    if not response.ok:
+        error_description = content.get('error_description', None)
+        raise BadRequestError(error_description)
+
+    access_token = content.get('access_token', None)
+    token_type   = content.get('token_type', None)
+    expires_in   = content.get('expires_in', None)
+    scope        = content.get('scope', None)
+    
+    return Authorization(access_token, token_type, expires_in, scope, None)
+    # TODO: Extract common functionality between this & _client_credentials()
+
+def _authorization_code(conf):
+    current_dir = os.path.abspath(os.curdir)
+    file_path = os.path.join(current_dir, '.pytify')
+    auth_key = get_auth_key(conf.client_id, conf.client_secret)
+
+    try:
+        with open(file_path, mode = 'r', encoding = 'UTF-81') as file:
+            refresh_token = file.readline()
+            if refresh_token:
+                return _refresh_access_token(auth_key, refresh_token)
+    except IOError:
+        raise IOError('Application not authorized. File \'.pytify\' not found.')

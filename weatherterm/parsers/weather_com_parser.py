aa153da3780b132916b0b8394d8ec5f29ea28493
Michael Santos <michael.joseph.santos@gmail.com>
2020-07-28 17:52:18 -0400

    Fixed today's forecast

--- weatherterm/parsers/weather_com_parser.py
+++ weatherterm/parsers/weather_com_parser.py
@@ -10,45 +10,45 @@ class WeatherComParser:
             ForecastType.TENDAYS: self._five_and_ten_days_forecast,
             ForecastType.WEEKEND: self._weekend_forecast
         }
-
-        self._base_url = 'http://weather.com/weather/{forecast_option}/l/{area_code}'
-        self._request = Request(self._base_url)
-
-        self._temp_regex = re.compile(r'([0-9]+)\D{,2}([0-9]+)')
+        self._base_url          = 'http://weather.com/weather/{forecast_option}/l/{area_code}'
+        self._request           = Request(self._base_url)
+        self._temp_regex        = re.compile(r'([0-9]+)\D{,2}([0-9]+)')
         self._only_digits_regex = re.compile('[0-9]+')
-        
-        self._unit_converter = UnitConverter(Unit.FAHRENHEIT)
+        self._unit_converter    = UnitConverter(Unit.FAHRENHEIT)
+
+    def run(self, args):
+        # Main method that parses weather info based on the selected forecast option
+        self._forecast_type = args.forecast_option
+        forecast_function = self._forecast[args.forecast_option]
+        return forecast_function(args)
 
     def _today_forecast(self, args):
-        criteria = {
-            'today_nowcard-temp': 'div',
-            'today_nowcard-phrase': 'div',
-            'today_nowcard-hilo': 'div'
-        }
-
         content = self._request.fetch_data(
             args.forecast_option.value,
             args.area_code
         )
         bs = BeautifulSoup(content, 'html.parser')
-        container = bs.find('section', class_ = 'today_nowcard-container')
         
-        weather_conditions = self._parse(container, criteria)
+        current_conditions_container = bs.find("div", {"data-testid": "CurrentConditionsContainer"})
+        criteria = [
+            ('span', 'data-testid', 'TemperatureValue'),
+            ('div', 'data-testid', 'wxPhrase'),
+            ('div', 'class', 'tempHiLoValue')
+        ]
+        weather_conditions = self._parseNew(current_conditions_container, criteria)
 
         if len(weather_conditions) < 1:
             raise Exception('Could not parse weather forecast for today.')
 
         weatherinfo = weather_conditions[0]
-        
-        temp_regex = re.compile (r'H\s+(\d+|\-{,2}).+L\s+(\d+|\-{,2})')
-        temp_info = temp_regex.search(weatherinfo['today_nowcard-hilo'])
-        high_temp, low_temp = temp_info.groups()
 
-        side = container.find('div', class_ = 'today_nowcard-sidecar')
-        wind, humidity = self._get_additional_info(side)
+        temp_info = weatherinfo['tempHiLoValue'].split('/')
+        high_temp = self._clear_str_number(temp_info[0])
+        low_temp  = self._clear_str_number(temp_info[1])
 
-        curr_temp = self._clear_str_number(weatherinfo['today_nowcard-temp'])
-
+        details_container = bs.find("section", { "data-testid" : "TodaysDetailsModule"})
+        wind, humidity = self._get_additional_info(details_container)
+        curr_temp = self._clear_str_number(weatherinfo['TemperatureValue'])
         self._unit_converter.dest_unit = args.unit
 
         td_forecast = Forecast(
@@ -57,7 +57,7 @@ class WeatherComParser:
             wind,
             high_temp   = self._unit_converter.convert(high_temp),
             low_temp    = self._unit_converter.convert(low_temp),
-            description = weatherinfo['today_nowcard-phrase']
+            description = weatherinfo['wxPhrase']
         )
 
         return [td_forecast]
@@ -101,11 +101,6 @@ class WeatherComParser:
 
         return self._prepare_data(results, args)
 
-    def run(self, args):
-        self._forecast_type = args.forecast_option
-        forecast_function = self._forecast[args.forecast_option]
-        return forecast_function(args)
-
     def _get_data(self, container, search_items):
         scraped_data = {}
 
@@ -116,22 +111,51 @@ class WeatherComParser:
                 scraped_data[key] = data
 
         return scraped_data
-    
+
+    def _get_dataNew(self, container, search_items):
+        # Called by _parse() to return a list of controls in the DOM container that match
+        # the search_items dictionary (key = data-testid/class name, value = DOM object)
+        scraped_data = {}
+
+        for tuple in search_items:
+            result = container.find(tuple[0], {"class": re.compile(tuple[2])}) \
+                if tuple[1] == 'class' \
+                else container.find(tuple[0], {tuple[1]: tuple[2]})
+            
+            data = None if result is None else result.get_text()
+            if data is not None:
+                scraped_data[tuple[2]] = data
+
+        return scraped_data
+
     def _parse(self, container, criteria):
         results = [
             self._get_data(item, criteria) for item in container.children
         ]
         return [result for result in results if result]
 
+    def _parseNew(self, container, criteria):
+        results = [
+            self._get_dataNew(item, criteria) for item in container.children
+        ]
+        return [result for result in results if result]
+
     def _clear_str_number(self, str_number):
+        # Clears out non-numeric characters from str_number
         result = self._only_digits_regex.match(str_number)
         return '--' if result is None else result.group()
 
     def _get_additional_info(self, content):
-        data = tuple(
-            item.td.span.get_text() for item in content.table.tbody.children
-        )
-        return data[:2]
+        # Parses the "Weather Today in {location}"" card (TodaysDetailsModule)
+        wind = content \
+            .find("span", {"data-testid":"Wind"}) \
+            .get_text()
+        # TODO: Infer wind direction based on style transformation (Ex: 270deg = E)
+        # <svg [...] style="transform:rotate(270deg)" [...] name="wind-direction" [...]</svg>
+        humidity = content \
+            .find("span", {"data-testid": "PercentageValue"}) \
+            .get_text()
+        return (wind, humidity)
     
     def _parse_list_forecast(self, content, args):
         criteria = {
